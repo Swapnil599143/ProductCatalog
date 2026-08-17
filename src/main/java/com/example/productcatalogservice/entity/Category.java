abd342d382d9d22830e5453d3f3bed86c1d29cdc
Swapnil Bharude <58132787+Swapnil341995@users.noreply.github.com>
2026-08-17 20:48:17 +0530

    first commit add category and product entities

---
+++ src/main/java/com/example/productcatalogservice/entity/Category.java
@@ -0,0 +1,9 @@
+package com.example.productcatalogservice.entity;
+
+import java.util.List;
+
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+    private List<Product> products;
+}
---
+++ src/main/java/com/example/productcatalogservice/entity/Product.java
@@ -0,0 +1,12 @@
+package com.example.productcatalogservice.entity;
+
+public class Product {
+    private Long productId;
+    private String productName;
+    private String description;
+    private Integer quantity;
+    private Double price;
+    private Double specialPrice;
+    private Double discount;
+    private Category category;
+}
